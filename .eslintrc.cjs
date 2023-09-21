95502460434dff5cebaf0ba06e538c7c1c927447
Menghour Say <saymenghour@gmail.com>
2023-09-22 00:20:45 +0700

    chore: config eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vue/vue3-recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
