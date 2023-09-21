8d70b32acd9c7c22b4c736734b9cf400daea4aa8
Menghour Say <saymenghour@gmail.com>
2023-09-22 00:51:35 +0700

    feat: add env
    In the Vite.js build tool, the .env.local file takes precedence over the .env file. This means that if both files exist, the variables defined in .env.local will override the values defined in .env.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }: UserConfig) => {
+  const env = loadEnv(mode ?? "development", process.cwd(), "");
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    define: {
+      "process.env": env,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
