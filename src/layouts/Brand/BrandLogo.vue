bc88cb5ee6471201fbf39af7e85d59925812a735
Menghour Say <saymenghour@gmail.com>
2024-02-04 14:38:33 +0700

    chore: update dashboard UI

--- src/layouts/Brand/BrandLogo.vue
+++ src/layouts/Brand/BrandLogo.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="text-2xl mb-7 flex justify-center items-center">
-    Vite App
+    <img
+      :src="vue"
+      alt="Logo"
+      class="w-8"
+    >
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import vue from "@/assets/images/logo/vue.svg";
+</script>
 
 <style scoped></style>
