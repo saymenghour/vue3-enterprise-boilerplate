22d0d3bdd6a509ee63e062757ebb5e22b581c027
Menghour Say <saymenghour@gmail.com>
2024-05-25 17:00:54 +0700

    fix: update validation schema optional allow empty string

--- src/modules/user-management/user/pages/UserCreate.vue
+++ src/modules/user-management/user/pages/UserCreate.vue
@@ -60,7 +60,7 @@
               :label="t('label.username')"
             />
           </Col>
-        <!-- <Col
+          <!-- <Col
           :md="12"
         >
           <div class="flex items-center h-full">
@@ -116,43 +116,43 @@ import { AppRoute } from '@/constants';
 const { t } = useI18n();
 const { success } = useNotification();
 
-const formSchema = toTypedSchema(z
-    .object({
-      lastNameKh: z.string().max(50).trim().optional(),
-      firstNameKh: z.string().max(50).trim().optional(),
-      lastName: z.string().max(35).trim(),
-      firstName: z.string().max(35).trim(),
-      phoneNumber: z.string().min(9).max(15).trim().optional(),
-      email: z.string().email().max(50).trim().optional(),
-      username: z
-        .string()
-        .min(4, { message: 'Username must be at least 4 characters long.' })
-        .max(35, { message: 'Username must be no more than 35 characters long.' })
-        .refine((value) => !value.includes(' '), { message: 'Username cannot contain spaces.' })
-        .refine((value) => /^[a-zA-Z0-9.]+$/.test(value), { message: 'Username can only contain letters, numbers, and dots.' }),
-      password: z
-        .string()
-        .min(8, 'Password must be at least 8 characters long.')
-        .max(50, 'Password must be no more than 50 characters long.')
-        .regex(/[a-zA-Z]/, 'Password must contain at least one letter.')
-        .regex(/\d/, 'Password must contain at least one number.')
-        .trim(),
-      confirmPassword: z
-        .string()
-        .min(8, 'Confirm password must be at least 8 characters long.')
-        .max(50, 'Confirm password must be no more than 50 characters long.')
-        .regex(/[a-zA-Z]/, 'Confirm password must contain at least one letter.')
-        .regex(/\d/, 'Confirm password must contain at least one number.')
-        .trim()
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-      message: "Passwords don't match.",
-      path: ['confirmPassword']
-    })
+const validationSchema = toTypedSchema(z
+  .object({
+    lastNameKh: z.string().max(50).optional().or(z.literal('')),
+    firstNameKh: z.string().max(50).optional().or(z.literal('')),
+    lastName: z.string().max(35),
+    firstName: z.string().max(35),
+    phoneNumber: z.string().min(9).max(15).optional().or(z.literal('')),
+    email: z.string().email().max(50).optional().or(z.literal('')),
+    username: z
+      .string()
+      .min(4, { message: 'Username must be at least 4 characters long.' })
+      .max(35, { message: 'Username must be no more than 35 characters long.' })
+      .refine((value) => !value.includes(' '), { message: 'Username cannot contain spaces.' })
+      .refine((value) => /^[a-zA-Z0-9.]+$/.test(value), { message: 'Username can only contain letters, numbers, and dots.' }),
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters long.')
+      .max(50, 'Password must be no more than 50 characters long.')
+      .regex(/[a-zA-Z]/, 'Password must contain at least one letter.')
+      .regex(/\d/, 'Password must contain at least one number.')
+      .trim(),
+    confirmPassword: z
+      .string()
+      .min(8, 'Confirm password must be at least 8 characters long.')
+      .max(50, 'Confirm password must be no more than 50 characters long.')
+      .regex(/[a-zA-Z]/, 'Confirm password must contain at least one letter.')
+      .regex(/\d/, 'Confirm password must contain at least one number.')
+      .trim()
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match.",
+    path: ['confirmPassword']
+  })
 );
 
 const { handleSubmit } = useForm({
-  validationSchema: formSchema
+  validationSchema
 });
 
 const { isPending, mutate } = useMutation({
