3a6bba3d29c06987f4355b30d0ee7e96c2760161
Menghour Say <saymenghour@gmail.com>
2024-06-16 15:37:24 +0700

    feat: disabled TanStack Query automatically requests when Window Focus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { VueQueryPlugin } from '@tanstack/vue-query';
+import { VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query';
 
 import '@/assets/styles/main.css';
 
@@ -13,10 +13,20 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
 
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+};
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
-app.use(VueQueryPlugin);
+app.use(VueQueryPlugin, vueQueryPluginOptions);
 app.use(ElementPlus);
 app.mount('#app');
