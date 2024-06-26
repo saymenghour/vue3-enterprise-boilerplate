fc9f28855284acf6f04bcda25fc7401ad8d7070b
Menghour Say <saymenghour@gmail.com>
2024-06-27 00:16:21 +0700

    fix: update schema to support record value is undefined

--- src/modules/user-management/role/roleSchema.ts
+++ src/modules/user-management/role/roleSchema.ts
@@ -5,5 +5,5 @@ export const roleValidationSchema = z.object({
   nameKh: z.string().min(3).max(100),
   type: z.string().max(100),
   description: z.string().max(255).optional().or(z.literal('')).nullable(),
-  permission: z.record(z.string(), z.array(z.string())).optional().nullable()
+  permission: z.record(z.string(), z.union([z.array(z.string()), z.undefined()])).optional().nullable()
 });
